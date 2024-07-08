23479bf8a3842bf1120aabe431aca1456f48a972
Campbell Blackburn <campbell.blackburn@x-rd.com.au>
2024-07-08 20:15:54 +1000

    Base framework for user interface

--- fan_website/fan_page.py
+++ fan_website/fan_page.py
@@ -1,8 +1,14 @@
 """ Fan page for the fan controller project """
 
+import json
+
+import requests
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 from todo import todos
+
+# Do not use localhost - werkzeug does weird things with ipv6
+SERVER_NAME = "http://127.0.0.1:8000"
 
 app = Flask(__name__)
 
@@ -25,7 +31,40 @@ def cleo_page():
 @app.route("/")
 def home_page():
     """Fan Home Page"""
-    return render_template("index.html")
+    response = requests.get(
+        f"{SERVER_NAME}/devices", headers={"accept": "application/json"}
+    ).json()
+
+    devices = [device["name"] for device in response]
+    devices.insert(0, "Select Device")
+    print(devices)
+    return render_template("index.html", devices=devices)
+
+
+@app.route("/commands")
+def command_list():
+    """Commands elements"""
+    print("command Called")
+    device_name = request.args.get("device")
+    if not device_name or device_name == "Select Device":
+        return render_template("commands.j2", commands=[])
+    print("Get data")
+    response = requests.get(
+        f"{SERVER_NAME}/devices/{device_name}/capabilities",
+        headers={"accept": "application/json"},
+    ).json()
+    print("returning response")
+    return render_template("commands.j2", commands=response)
+
+
+@app.route("/submit", methods=["POST"])
+def submit_command():
+    requests.post(
+        f"{SERVER_NAME}/devices/{request.form["device"]}/commands",
+        headers={"accept": "application/json"},
+        json={"device_id": request.form["device"], "capability_id": request.form["command"]}
+    )
+    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
 
 
 @app.route("/form")
